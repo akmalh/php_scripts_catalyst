02b1056f3f8fe60f96085d6560f989caaaa496b8
Ubuntu <ubuntu@php-server.novalocal>
2016-02-08 08:05:47 +0000

    Tested DB connect

--- user_upload.php
+++ user_upload.php
@@ -56,6 +56,23 @@
         	fclose($file);
 	}
 
+	function connectDB(){
 	
+		$servername = "localhost";
+		$username = "root";
+		$password = "password";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password);
+
+		// Check connection
+		if ($conn->connect_error) {
+    		die("Connection failed: " . $conn->connect_error."\n\n");
+		}
+		echo "Connected successfully\n";
+
+		$conn->close();
+	}	
 	commandCheck($argv);
+	connectDB();
 ?>

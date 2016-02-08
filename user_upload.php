8e4d0618a6f94e2e608bbac0326340d2f605271d
Ubuntu <ubuntu@php-server.novalocal>
2016-02-08 08:40:24 +0000

    Tested DB creation

--- user_upload.php
+++ user_upload.php
@@ -71,8 +71,36 @@
 		}
 		echo "Connected successfully\n";
 
+		// Create database
+		$sql = "CREATE DATABASE IF NOT EXISTS myDB";
+		if ($conn->query($sql) === TRUE) {
+    			echo "Database created successfully\n\n";
+		} 
+		else {
+    			echo "Error creating database: " . $conn->error. "\n\n";
+		}
+
+		// sql to create table
+		$sql = "CREATE TABLE IF NOT EXISTS users (
+			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			name VARCHAR(30) NOT NULL,
+			surname VARCHAR(30) NOT NULL,
+			email VARCHAR(50),
+			insert_date TIMESTAMP,
+			UNIQUE (email)
+			)";
+
+		if ($conn->query($sql) === TRUE) {
+    			echo "Table users created successfully";
+		} 
+		else {
+    			echo "Error creating table: " . $conn->error;
+		}
+
 		$conn->close();
-	}	
+	}
+
+		
 	commandCheck($argv);
 	connectDB();
 ?>
